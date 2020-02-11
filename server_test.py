5671af766f8d1ec0bc1fcd3108a03be35eb8c1fc
errikosmes <errikos.messara@gmail.com>
2020-02-11 17:28:09 +0100

    basic TCP with pickle working

--- server_test.py
+++ server_test.py
@@ -4,24 +4,26 @@ import socket, pickle, os, time, subprocess
 """
 myIP = subprocess.check_output('hostname -I', shell=True).decode('utf-8')
 myIP = myIP[:-2]
-"""
+
 
 print("[INFO] Server IP", myIP)
+"""
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET=IPV4, SOCK_STREAM=TCP
-s.bind((myIP, 1212))
+s.bind((socket.gethostname(), 1214))
 s.listen(5) #accepting 5 connections max
+
+clientsocket, address = s.accept()
+print("Connection from {} has been established!".format(address))
 
 
 while True:
 	mtx = np.random.randint(low=0, high=1000, size=(4, 7), dtype=np.int16)
-	print("\n", mtx)
-
 	mtx_dump = pickle.dumps(mtx)
 
-	clientsocket, address = s.accept()
-	print("Connection from {} has been established!".format(address))
+	
 	clientsocket.send(mtx_dump)
+	print("\n", mtx)
 
-	time.sleep(5)
+	time.sleep(0.1)
 	
