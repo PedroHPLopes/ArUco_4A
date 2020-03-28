533013eea8635cbece6ec1e798e392ea1b81d8ba
errikosmes <errikos-panagiotis.messara@etu.univ-grenoble-alpes.fr>
2020-03-28 19:29:46 +0200

    new north-south test

---
+++ nord_sud_test.py
@@ -0,0 +1,23 @@
+from random import seed
+from random import randint
+import time, serial
+
+sleep_time = 2
+serial_port = "/dev/ttyS0"
+
+def send_north():
+    ser.write("1")
+    print("Sent North ")
+
+def send_south():
+    ser.write("0")
+    print("Sent South")
+
+seed(1)
+ser = serial.Serial(port=serial_port, baudrate=115200, parity=serial.PARITY_NONE, bytesize=serial.EIGHTBITS, timeout=10)
+
+while True:
+    if randint(0, 1): send_north()
+    else: send_south()
+    
+    time.sleep(sleep_time)
