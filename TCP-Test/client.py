ae0c3cec78464f6c6d2696fdc54cd8761adea551
PedroHPLopes <pedrohplopes98@gmail.com>
2020-04-26 21:08:23 +0200

    Cliens can be launched before the server, and they reconnect automatically

--- TCP-Test/client.py
+++ TCP-Test/client.py
@@ -3,49 +3,45 @@ import socket, pickle, os, time, subprocess, select
 import errno
 import sys
 
-my_username = "ROBO 1"
-HEADER_LENGTH = 10
 
+my_username = "ROBO 1"
 IP = "127.0.0.1"
 PORT = 1234
 
-'''
-    mtx = np.random.randint(low=0, high=10, size=(4, 7), dtype=np.int16)
-    mtx_dump = pickle.dumps(mtx)
-    message = mtx_dump
-'''
-# Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def connect(IP,PORT,my_username):
+    """
+    Tries to connect to the server. If connected, sends the username
+    Returns client_socket
+    """
+    connected = False
+    while not connected:
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((IP, PORT))
+            client_socket.setblocking(False)
+            username = my_username.encode('utf-8')
+            client_socket.send(username)
+            connected = True
+        except socket.error:
+            continue
+    return client_socket
 
-# Connect to a given ip and port
-client_socket.connect((IP, PORT))
-
-# Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
-client_socket.setblocking(False)
-
-# Prepare username and header and send them
-# We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
-username = my_username.encode('utf-8')
-client_socket.send(username)
+client_socket = connect(IP,PORT,my_username)
 
 while True:
     try:
         mtx = np.random.randint(low=0, high=10, size=(4, 7), dtype=np.int16)
         mtx_d = pickle.dumps(mtx)
         client_socket.send(mtx_d)
-        time.sleep(2) 
+        time.sleep(0.1)
+        # Sans ce time.sleep() le serveur tombe. Il nous donne cet erreur:
+        #UnicodeDecodeError: 'utf-8' codec can't decode byte 0x80 in position 6: invalid start byte
+        # Je n'arrive pas a trouver une autre  solution                   
         while True:
-            mtx_r = client_socket.recv(1024)
-            try:
-                mtx_a_r = pickle.loads(mtx_r)
-                print("Robo 2:")
-                print(mtx_a_r)
-            except:
-                print("Fail on pickle loads")
-
-
+            mtx_r = client_socket.recv(1024) 
+            mtx_a_r = pickle.loads(mtx_r)
+            print("Received from Robo 2:")
+            print(mtx_a_r)
     except IOError as e:
             # This is normal on non blocking connections - when there are no incoming data error is going to be raised
             # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
@@ -53,18 +49,21 @@ while True:
             # If we got different error code - something happened
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
             print('Reading error: {}'.format(str(e)))
-            sys.exit()
+            print('Lost connection, trying to reconnect')
+            client_socket = connect(IP,PORT,my_username)
+
 
             # We just did not receive anything
         continue
-
+    except KeyboardInterrupt as e:
+        print('Keyboard Interrupr')
+        sys.exit()
+        
     except Exception as e:
             # Any other exception - something happened, exit
-        print('Reading error: '.format(str(e)))
-        sys.exit()
-    except KeyboardInterrupt:
-        client_socket.close()
-
+        print('Error: {}'.format(str(e)))
+        print('Lost connection, trying to reconnect')
+        client_socket = connect(IP,PORT,my_username)
 
 
 
